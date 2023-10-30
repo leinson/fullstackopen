128f3c44d89c70775d65006d430af80fde375259
leinson <hannah.leinson@gmail.com>
2023-10-30 13:07:17 +0200

    structure skeleton, the logger, config and models separated from index.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,23 +1,25 @@
-require('dotenv').config()
+
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
 
+// const blogSchema = new mongoose.Schema({
+//     title: String,
+//     author: String,
+//     url: String,
+//     likes: Number
+// })
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+// const Blog = mongoose.model('Blog', blogSchema)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
+//const mongoUrl = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -40,7 +42,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+//const PORT = process.env.PORT
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
