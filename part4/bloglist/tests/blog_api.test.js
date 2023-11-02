80f245f9da543b3264a4dc9ba6a9f902ffae7b9f
leinson <hannah.leinson@gmail.com>
2023-11-02 12:30:23 +0200

    exercise 4.11

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -19,56 +19,77 @@ const initialBlogs = [
   }
 ]
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
-})
+describe('when there is blogs already saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('there is two blogs', async () => {
-  const response = await api.get('/api/blogs')
+  test('there is two blogs', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlogs.length)
-})
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
 
-test('id-field is named id and not _id', async () => {
-  const response = await api.get('/api/blogs')
+  test('id-field is named id and not _id', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body[0].id).toBeDefined()
-})
+    expect(response.body[0].id).toBeDefined()
+  })
 
+  describe('when a new blog is added', () => {
 
-test('a blog can be added ', async () => {
-  const newBlog = {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2
-  }
+    test('it succeeds and content is correct ', async () => {
+      const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const response = await api.get('/api/blogs')
+      const response = await api.get('/api/blogs')
 
-  const titles = response.body.map(r => r.title)
+      const titles = response.body.map(r => r.title)
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(titles).toContain(
-    'Type wars'
-  )
+      expect(response.body).toHaveLength(initialBlogs.length + 1)
+      expect(titles).toContain(
+        'Type wars'
+      )
+    })
+    test('if there is no likes value, it is set to zero', async () => {
+      const newBlog = {
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+
+      expect(response.body[2].likes).toBe(0)
+    })
+  })
 })
 
 afterAll(async () => {
