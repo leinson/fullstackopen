55419b22ecd9cc1e4d8cacc46e7a2c286f23ae8b
leinson <hannah.leinson@gmail.com>
2023-11-27 14:55:35 +0200

    exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,8 +6,10 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 logger.info('connecting to', config.MONGODB_URI)
@@ -22,8 +24,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
