afeaaf85a73fb9e44054281ec5bd05b11ce49286
leinson <hannah.leinson@gmail.com>
2023-12-11 15:37:22 +0200

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const hideWhenShowDetails = { display: showDetails ? 'none' : '' }
+  const showWhenShowDetails = { display: showDetails ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 3,
+    paddingBottom: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    marginLeft: 3
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenShowDetails}>
+       {blog.title} - {blog.author}
+       <button onClick={ () => setShowDetails(true)}>view</button>
+      </div>
+      <div style={showWhenShowDetails}>
+        {blog.title} - {blog.author}
+        <button onClick={ () => setShowDetails(false)}>hide</button>
+        <br/>
+        {blog.url}
+        <br/>
+        likes {blog.likes} <button>Like</button>
+        <br/>
+        {blog.user.name}        
+      </div>
+    </div>  
+  )
+  }
 
 export default Blog
